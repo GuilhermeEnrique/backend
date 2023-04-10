0ca450fe8e396fb034b7d162a1a899bbe61ccf22
GuilhermeEnrique <guilherme.dantas416@gmail.com>
2023-04-10 12:02:20 -0400

    configurações no banco de dados

--- prisma/migrations/20230329155943_campo_imagem/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `icon_personagem` to the `Personagem` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Personagem" ADD COLUMN     "icon_personagem" TEXT NOT NULL;
